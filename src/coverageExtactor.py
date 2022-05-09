d2427fabee1d57e19b755008e9c594b6ddfea834
usmansiddique-rs <usman.siddique@rapidsilicon.com>
2022-05-09 17:19:05 +0500

    created function readCoverageTable

--- src/coverageExtactor.py
+++ src/coverageExtactor.py
@@ -13,24 +13,18 @@ os.system('clear')
 
 class coverageExtractor:
     # ----------------------------------------------------------------- Variables
-    # public vars
-    htmlFilePath = str()
-    htmlFileName = str()
-    # protected vars
-    _covTablesDF = str()
-    # private vars
-    
-    # ----------------------------------------------------------------- Functions
     def __init__(self):
         # public vars
-        self.htmlFilePath = ''
-        self.htmlFileName = ''
-        self.xlsxFileName = ''
+        self.htmlFilePath = str()
+        self.htmlFileName = str()
+        self.xlsxFileName = str()
         # protected vars
-        self._covTablesDF = ''
+        self._covTablesDF = str()
+        self._isolatedDfObj = pd.DataFrame
         # private vars
-        
-        
+    
+    
+    # ----------------------------------------------------------------- Functions
         logging.basicConfig(level=logging.DEBUG, filename='coverageExtractor.log',
                             format='%(asctime)s | %(filename)s | %(funcName)s | %(levelname)s | %(lineno)d | %(message)s')
     
@@ -40,7 +34,8 @@ class coverageExtractor:
         self._covTablesDF = pd.read_html(self.htmlFilePath,header=[0])
         logging.info('Total tables in HTML doc = ' + str(len(self._covTablesDF)))
         for i in range(len(self._covTablesDF)):
-            logging.info('Table[' + str(i) + '] col ' + str(self._covTablesDF[i].columns))
+            logging.info('Table "{}" columns: "{}"'.format(i,str(self._covTablesDF[i].columns)))
+            # logging.info('Table[' + str(i) + '] col ' + str(self._covTablesDF[i].columns))
     
     
     # print a table from excel or html file
@@ -68,8 +63,24 @@ class coverageExtractor:
             logging.info('Added Table "{}" as Sheet "{}" in doc "{}"'.format(index,tempSheetName,self.xlsxFileName))
     
     
-    
-
+    def readCoverageTable(self,columnName,keyword):
+        # iterate through all dataframes
+        for index, table in enumerate(self._covTablesDF):
+            # find table with specific column name
+            if columnName in table:
+                # self.displayDF(table)
+                logging.info('FOUND column heading "{}" in "Cov Table {}"'.format(columnName,index))
+                # append column names to new data frame
+                logging.info('Table column headings "{}"'.format(table.columns))
+                self._isolatedDfObj = pd.DataFrame(columns=table.columns)
+                # find rows with specific column value (string)
+                tempRow = table.loc[table[columnName] == keyword]
+                # find rows with specific column value (array)
+                # tempRow = table.loc[table[columnName].isin(keyword)]
+                # append tempRow to new data frame
+                self._isolatedDfObj = self._isolatedDfObj.append(tempRow)
+                # print('/////////////////////////////////////////////////////////////////////////////')
+                # self.displayDF(self._isolatedDfObj)
 
 
 # ===========================================================================================
