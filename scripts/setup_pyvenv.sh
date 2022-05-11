3f1b4fcfab2c7c969471635de1305a8273ca2069
usmansiddique-rs <usman.siddique@rapidsilicon.com>
2022-05-11 12:04:05 +0500

    updated script setup_venv to create venv using pip package

--- scripts/setup_pyvenv.sh
+++ scripts/setup_pyvenv.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-python3 -m venv .pyvenv
+# check if pip package virtualenv is installed or not
+CHECK1=$(virtualenv --version)
+CHECK1=$(echo $CHECK1 | awk '{print $1}')
+
+if [[ $CHECK1 = 'virtualenv' ]]
+then
+    echo -e "PIP package virtualenv INSTALLED. \n"
+else
+    echo -e "INSTALLING PIP package virtualenv"
+    python3 -m pip install virtualenv
+fi
+
+# create virtual env
+virtualenv .pyvenv
 source .pyvenv/bin/activate
 pip install --upgrade pip
 echo -e "\n"
@@ -9,3 +22,4 @@ echo -e "\n"
 python3 -m pip install -r requirements.txt
 echo -e "\n"
 pip list
+echo -e "\n"
